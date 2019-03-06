3a17bfbaefa49dd712f5e73c4bd7130d0bc71597
HOS <hos@ukr.net>
2019-03-07 00:49:21 +0200

    END

--- src/Lesson01/Test.java
+++ src/Lesson01/Test.java
@@ -2,6 +2,6 @@ package Lesson01;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.format("Hello Topjava Enterprise!");
+        System.out.format("Hello!");
     }
 }

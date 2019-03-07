b2576257095d26c12e4bf76a3371dd1912d2877d
HOS <hos@ukr.net>
2019-03-07 20:52:16 +0200

    begin

---
+++ src/Lesson04/Main.java
@@ -0,0 +1,13 @@
+package Lesson04;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] x = new int[28];
+        for (int i = 0; i < x.length; i++) {
+            x[i] = i * 635;
+            if (i==0 || i==14 || i==27){
+                System.out.println(x[i]);
+            }
+        }
+    }
+}

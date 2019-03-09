c6b6af1bef0953487c47ec987ad4440520eeac66
HOS <hos@ukr.net>
2019-03-09 14:58:35 +0200

    End

--- src/Lesson04/Main.java
+++ src/Lesson04/Main.java
@@ -2,11 +2,24 @@ package Lesson04;
 
 public class Main {
     public static void main(String[] args) {
+
         int[] x = new int[28];
+
         for (int i = 0; i < x.length; i++) {
             x[i] = i * 635;
-            if (i==0 || i==14 || i==27){
-                System.out.println(x[i]);
+
+            Integer result = (i == 0 || i == 14 || i == 27) ? x[i] : null; // это не правильная запись
+            if (result != null){
+                System.out.println(result);
+
+/*
+Это правильная запись
+            switch (i){
+                case 0:
+                case 14:
+                case 27:
+                    System.out.println(x[i]);
+*/
             }
         }
     }

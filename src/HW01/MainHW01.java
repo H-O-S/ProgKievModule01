470f62b298f43393284e7a1697c2ae6bd367ba49
HOS <hos@ukr.net>
2019-03-10 10:52:41 +0200

    Partially implemented

---
+++ src/HW01/MainHW01.java
@@ -0,0 +1,18 @@
+/**
+ * Java Start. Home Work 01.
+ *
+ * @author Oleg Khrulov
+ * @version dated 29.02.2019
+ */
+package HW01;
+
+public class MainHW01 {
+    public static void main(String[] args) {
+        Runtime r = Runtime.getRuntime();
+//        System.out.println(System.getProperty(“os.name”));
+//        System.out.println(System.getProperty(“os.version”));
+        System.out.println(r. availableProcessors());
+// total amount of memory in the Java virtual machine.
+        System.out.println(r. totalMemory());
+    }
+}

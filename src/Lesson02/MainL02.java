a18712bbbd57f3d4b1f87ff21e0d892ad2f46ade
HOS <hos@ukr.net>
2019-03-11 13:30:56 +0200

    added class blank for Lecture 02 and Lecture 03

---
+++ src/Lesson02/MainL02.java
@@ -0,0 +1,13 @@
+/**
+ * Java Start. Lesson 02.
+ *
+ * @author Oleg Khrulov
+ * @version dated 28.02.2019
+ */
+package Lesson02;
+
+public class MainL02 {
+    public static void main(String[] args) {
+        System.out.format("Hello!");
+    }
+}
---
+++ src/Lesson03/MainL03.java
@@ -0,0 +1,13 @@
+/**
+ * Java Start. Lesson 03.
+ *
+ * @author Oleg Khrulov
+ * @version dated 05.03.2019
+ */
+package Lesson03;
+
+public class MainL03 {
+    public static void main(String[] args) {
+        System.out.format("Hello!");
+    }
+}

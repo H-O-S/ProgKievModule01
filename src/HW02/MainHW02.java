09c3ffc7f786606fbb63649649a97963634e8e10
HOS <hos@ukr.net>
2019-03-11 15:20:52 +0200

    Completed task 1 from homework 02

--- src/HW02/MainHW02.java
+++ src/HW02/MainHW02.java
@@ -2,12 +2,50 @@
  * Java Start. Home Work 02.
  *
  * @author Oleg Khrulov
- * @version dated 10.03.2019
+ * @version dated 11.03.2019
  */
 package HW02;
 
+import java.util.Scanner;
+
 public class MainHW02 {
     public static void main(String[] args) {
-        System.out.format("Hello!");
+        String s1 = null;
+        String s2 = null;
+        String s3 = null;
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 1; i <= 3; i++){
+            System.out.print("Enter string " + i + ": ");
+
+            switch (i){
+                case 1:
+                    s1 = scanner.nextLine();
+                    break;
+                case 2:
+                    s2 = scanner.nextLine();
+                    break;
+                case 3:
+                    s3 = scanner.nextLine();
+                    break;
+            }
+        }
+
+        System.out.println(s1 + s2);
+        System.out.println(s3 + s2 + s1);
+        System.out.println(s1 + s2 + s3);
+
     }
 }
+
+
+
+/*
+            if (i == 1) {
+                s1 = scanner.nextLine();
+            } else if (i == 2) {
+                s2 = scanner.nextLine();
+            } else if (i == 3) {
+                s3 = scanner.nextLine();
+            }
+*/

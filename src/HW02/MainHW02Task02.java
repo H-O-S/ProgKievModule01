e723bf7a8cdde69367500d4fe28bc49905a688a3
HOS <hos@ukr.net>
2019-03-11 15:52:41 +0200

    Completed task 2 from homework 02

---
+++ src/HW02/MainHW02Task02.java
@@ -0,0 +1,25 @@
+package HW02;
+
+import java.util.Scanner;
+
+public class MainHW02Task02 {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter x1: ");
+        int x1 = scanner.nextInt();
+        System.out.print("Enter x2: ");
+        int x2 = scanner.nextInt();
+
+        System.out.println("x1 + x2 = " + (x1 + x2));
+        System.out.println("x1 - x2 = " + (x1 - x2));
+        System.out.println("x1 * x2 = " + (x1 * x2));
+        System.out.println("x1 / x2 = " + (x1 / x2));
+        System.out.println("x1 % x2 = " + (x1 % x2));
+        System.out.println("x1++ + x2 = " + (x1++ + x2));
+        System.out.println("++x1 + x2 = " + (++x1 + x2));
+        System.out.println("x1 + --x2 = " + (x1 + --x2));
+        System.out.println("x1 + x2-- = " + (x1 + x2--));
+        System.out.println("x1 += 2 + x2 = " + (x1 += 2 + x2));
+    }
+}

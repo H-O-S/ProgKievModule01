bb6ab091c24857fa29852084cd081470366b11ef
HOS <hos@ukr.net>
2019-03-11 18:38:53 +0200

    Preparing class templates for homework 03

---
+++ src/HW03/MainHW03Task03.java
@@ -0,0 +1,13 @@
+/**
+ * Java Start. Home Work 03.
+ *
+ * @author Oleg Khrulov
+ * @version dated 11.03.2019
+ */
+package HW03;
+
+public class MainHW03Task03 {
+    public static void main(String[] args) {
+        System.out.format("Hello!");
+    }
+}
--- src/HW03/MainHW03Task05.java
+++ src/HW03/MainHW03Task05.java
@@ -2,11 +2,11 @@
  * Java Start. Home Work 03.
  *
  * @author Oleg Khrulov
- * @version dated 10.03.2019
+ * @version dated 11.03.2019
  */
 package HW03;
 
-public class MainHW03 {
+public class MainHW03Task05 {
     public static void main(String[] args) {
         System.out.format("Hello!");
     }

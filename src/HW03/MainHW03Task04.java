dedf49e74a13fc60286d5300b6713fc36d6602b7
HOS <hos@ukr.net>
2019-03-12 17:50:49 +0200

    Completed Task 4 From Homework 03

--- src/HW03/MainHW03Task04.java
+++ src/HW03/MainHW03Task04.java
@@ -6,8 +6,26 @@
  */
 package HW03;
 
+import java.util.Scanner;
+
+@SuppressWarnings("Duplicates")
 public class MainHW03Task04 {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter array length: ");
+        int arrayLength = scanner.nextInt();
+        int[] array = new int[arrayLength];
+        int arraySum = 0;
+
+        for (int i = 0; i < array.length; i++){
+            System.out.print("Enter an integer to fill cell the array[" + i + "]: ");
+            array[i] = scanner.nextInt();
+            arraySum += array[i];
+        }
+
+        System.out.println(arraySum / array.length);
+
+        scanner.close();
     }
 }

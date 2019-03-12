b1698f1029acfaabea4f3a0bc231291fb5c0ac09
HOS <hos@ukr.net>
2019-03-12 16:54:57 +0200

    Completed Task 2 From Homework 03

--- src/HW03/MainHW03Task02.java
+++ src/HW03/MainHW03Task02.java
@@ -6,8 +6,16 @@
  */
 package HW03;
 
+import java.util.Arrays;
+
 public class MainHW03Task02 {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        int[] array = {1,2,3,4,5};
+        int temp = array[0];
+
+        array[0] = array[array.length - 1];
+        array[array.length - 1] = temp;
+
+        System.out.println(Arrays.toString(array));
     }
 }

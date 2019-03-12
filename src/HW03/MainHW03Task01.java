e3f95d4a5051ef082c0c9168f1329f57fc8121f2
HOS <hos@ukr.net>
2019-03-12 15:29:23 +0200

    Completed Task 1 From Homework 03

--- src/HW03/MainHW03Task01.java
+++ src/HW03/MainHW03Task01.java
@@ -2,12 +2,27 @@
  * Java Start. Home Work 03.
  *
  * @author Oleg Khrulov
- * @version dated 11.03.2019
+ * @version dated 12.03.2019
  */
 package HW03;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class MainHW03Task01 {
     public static void main(String[] args) {
-        System.out.format("Hello!");
+
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[10];
+
+        for (int i = 0; i < array.length; i++){
+            System.out.print("Enter an integer to fill cell the array[" + i + "]: ");
+            array[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+
+        scanner.close();
     }
 }

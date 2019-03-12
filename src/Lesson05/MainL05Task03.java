a5039965ce997b1f64b2e59150f8336c5f76ef0b
HOS <hos@ukr.net>
2019-03-12 20:43:27 +0200

    Completed Task 3 From Lesson 05

---
+++ src/Lesson05/MainL05Task03.java
@@ -0,0 +1,17 @@
+/**
+ * Java Start. Lesson 05.
+ *
+ * @author Oleg Khrulov
+ * @version dated 12.03.2019
+ */
+package Lesson05;
+
+public class MainL05Task03 {
+    static void doJob(String x, int y, double z) {
+            System.out.println("S = " + x + y + z);
+    }
+
+    public static void main(String[] args) {
+        doJob("hello", 5, 6.3);
+    }
+}

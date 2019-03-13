1e24080706be7be2d74ba24a9ed7f33b31cc801f
HOS <hos@ukr.net>
2019-03-13 23:40:45 +0200

    Completed Task 4 From Homework 04

--- src/HW04/MainHW04Task04.java
+++ src/HW04/MainHW04Task04.java
@@ -15,6 +15,14 @@ package HW04;
 
 public class MainHW04Task04 {
     public static void main(String[] args) {
-        System.out.format("Hello!");
+
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
+        int sumArray = 0;
+
+        for (int i = 1; i < (array.length - 1); i++){
+            sumArray += array[i];
+        }
+
+        System.out.println(sumArray);
     }
 }

5e6626f36aa9f6f26428c85903a12bfdbc63b96a
HOS <hos@ukr.net>
2019-03-19 17:29:10 +0200

    Preparing class templates for HomeWork 06

---
+++ src/HW06/MainHW06Task05.java
@@ -0,0 +1,19 @@
+/**
+ * Java Start. Home Work 06.
+ *
+ * @author Oleg Khrulov
+ * @version dated 19.03.2019
+ */
+
+/*
+        Ввести с консоли число в бинарном формате. Перевести
+        его в int и вывести на экран (“10” -> 2).
+*/
+
+package HW06;
+
+public class MainHW06Task05 {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}

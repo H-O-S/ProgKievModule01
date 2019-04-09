f1fdb174e44b914134304162216ba89b0dbe8a59
HOS <hos@ukr.net>
2019-04-10 00:42:38 +0300

    Completed task 3 from homework 05

--- src/HW05/MainHW05Task03.java
+++ src/HW05/MainHW05Task03.java
@@ -2,7 +2,7 @@
  * Java Start. Home Work 05.
  *
  * @author Oleg Khrulov
- * @version dated 10.03.2019
+ * @version dated 10.04.2019
  */
 
 /*
@@ -12,8 +12,24 @@
 
 package HW05;
 
+import java.util.Arrays;
+
 public class MainHW05Task03 {
     public static void main(String[] args) {
-        System.out.format("Hello!");
+        char[] arrayOne = {'H', 'E', 'L', 'L', 'O'};
+        char[] arrayTwo = {'W','O', 'R', 'L', 'D'};
+
+        char[] arrayResult = mergeArrays (arrayOne, arrayTwo);
+
+        System.out.println(Arrays.toString(arrayResult));
+    }
+
+    static char[] mergeArrays(char[] arrayOne, char[] arrayTwo){
+        char[] arrayResult = new char[arrayOne.length + arrayTwo.length];
+
+        System.arraycopy(arrayOne, 0, arrayResult, 0, arrayOne.length);
+        System.arraycopy(arrayTwo, 0, arrayResult, arrayOne.length, arrayTwo.length);
+
+        return arrayResult;
     }
 }

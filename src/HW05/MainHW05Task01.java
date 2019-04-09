8c7ddbb466edbd039574459f5ace9c7591e12e77
HOS <hos@ukr.net>
2019-04-09 23:20:11 +0300

    Completed task 1 from homework 05

--- src/HW05/MainHW05Task01.java
+++ src/HW05/MainHW05Task01.java
@@ -2,7 +2,7 @@
  * Java Start. Home Work 05.
  *
  * @author Oleg Khrulov
- * @version dated 10.03.2019
+ * @version dated 09.04.2019
  */
 
 /*
@@ -14,8 +14,25 @@
 
 package HW05;
 
+import java.util.Arrays;
+
 public class MainHW05Task01 {
+    private static Integer[] array = {1, 2, 3, 4, 5, 6};
+    private static final String RESULT_SUM = "The sum of the first three elements of the modified array is equal to: %n%d%n%n";
+    private static final String RESULT_ARRAY = "Array after processing: %n%s%n";
+
     public static void main(String[] args) {
-        System.out.format("Hello!");
+        System.out.printf(RESULT_SUM + RESULT_ARRAY,
+                arrayProcessing(array),
+                Arrays.toString(array));
+    }
+
+    public static Integer arrayProcessing(Integer[] array){
+        int sum = 0;
+        for (int i = 0; i < 3; i++){
+            sum += ++array[i];
+        }
+
+        return sum;
     }
 }

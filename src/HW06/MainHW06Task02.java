2e2dc4bc125d1b2b00f2bd1200a8cfb5f63593ff
HOS <hos@ukr.net>
2019-04-10 15:14:22 +0300

    Completed task 2 from homework 06

--- src/HW06/MainHW06Task02.java
+++ src/HW06/MainHW06Task02.java
@@ -2,7 +2,7 @@
  * Java Start. Home Work 06.
  *
  * @author Oleg Khrulov
- * @version dated 19.03.2019
+ * @version dated 10.04.2019
  */
 
 /*
@@ -12,7 +12,29 @@
 package HW06;
 
 public class MainHW06Task02 {
+
+    static boolean equals(short[] a1, short[] a2){
+        if (a1.length != a2.length){
+            return false;
+        }
+
+        for (int i = 0; i < a1.length; i++){
+            if (a1[i] != a2[i]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        short[] a1 = {1, 2, 3, 4, 5};
+        short[] a2 = {1, 2, 3, 4, 5};
+        short[] a3 = {5, 4, 3, 2, 1};
+        short[] a4 = {1, 2, 3};
+
+        System.out.println(equals(a1, a2));
+        System.out.println(equals(a2, a3));
+        System.out.println(equals(a1, a4));
     }
 }

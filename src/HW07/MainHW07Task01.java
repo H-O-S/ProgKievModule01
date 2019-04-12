0acfad0252cccff8afdcfe44eee3c2ab81a07e77
HOS <hos@ukr.net>
2019-04-12 18:47:39 +0300

    Completed task 1 from homework 07

--- src/HW07/MainHW07Task01.java
+++ src/HW07/MainHW07Task01.java
@@ -2,7 +2,7 @@
  * Java Start. Home Work 07.
  *
  * @author Oleg Khrulov
- * @version dated 10.03.2019
+ * @version dated 12.04.2019
  */
 
 /*
@@ -14,6 +14,10 @@ package HW07;
 
 public class MainHW07Task01 {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        String input = "word1 word2 word3";
+        String[] words = input.split(" ");
+        String result = words[2] + " " + words[1] + " " + words[0];
+
+        System.out.println(result);
     }
 }

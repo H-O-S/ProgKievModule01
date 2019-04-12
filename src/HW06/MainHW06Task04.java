5ffaacfdf8b7e730904fad00c099a5062ca6e367
HOS <hos@ukr.net>
2019-04-12 12:40:55 +0300

    Completed task 4 from homework 06

--- src/HW06/MainHW06Task04.java
+++ src/HW06/MainHW06Task04.java
@@ -2,7 +2,7 @@
  * Java Start. Home Work 06.
  *
  * @author Oleg Khrulov
- * @version dated 19.03.2019
+ * @version dated 12.04.2019
  */
 
 /*
@@ -13,7 +13,56 @@
 package HW06;
 
 public class MainHW06Task04 {
+    static int setBit(int x, int pos){
+        return x | (1 << pos);
+    }
+
+    static boolean isBitSet(int x, int pos){
+        return (x & (1 << pos)) != 0;
+    }
+
+    static void setBit(int[] a, int pos){
+        int intN = a.length - (pos / 32) - 1;
+        int bitN = pos % 32;
+        a[intN] = setBit(a[intN], bitN);
+    }
+
+    static boolean isBitSet(int[] a, int pos){
+        int intN = a.length - (pos / 32) - 1;
+        int bitN = pos % 32;
+        return isBitSet(a[intN], bitN);
+    }
+
+    @SuppressWarnings("Duplicates")
+    static String toString(int[] array){
+        StringBuilder stringBuilder = new StringBuilder("int[] array [");
+
+        for (int i = 0; i < array.length; i++){
+            if (i < array.length - 1){
+                stringBuilder.append(array[i]).append(", ");
+            }
+            else {
+                stringBuilder.append(array[i]);
+            }
+        }
+
+        return stringBuilder.append("] ").toString();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        int[] array = {0, 0};
+
+        setBit(array, 0);
+        setBit(array, 1);
+        setBit(array, 32);
+        setBit(array, 63);
+
+        System.out.println(isBitSet(array, 0));
+        System.out.println(isBitSet(array, 1));
+        System.out.println(isBitSet(array, 32));
+        System.out.println(isBitSet(array, 63));
+        System.out.println(isBitSet(array, 55));
+
+        System.out.println(toString(array));
     }
 }

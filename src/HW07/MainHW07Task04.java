18bddaced7469705a231a4d566d4a29a4fd94531
HOS <hos@ukr.net>
2019-04-12 20:16:00 +0300

    Completed task 4 from homework 07

--- src/HW07/MainHW07Task04.java
+++ src/HW07/MainHW07Task04.java
@@ -2,7 +2,7 @@
  * Java Start. Home Work 07.
  *
  * @author Oleg Khrulov
- * @version dated 10.03.2019
+ * @version dated 12.04.2019
  */
 
 /*
@@ -16,6 +16,26 @@ package HW07;
 
 public class MainHW07Task04 {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        final String text = "I     am a long      sentence I am the     second sentence. Yes!";
+
+        StringBuilder stringBuilder = new StringBuilder();
+        String[] words = text.split(" ");
+
+        for (int i = 0; i < words.length; i++){
+            if (words[i].length() > 0){
+                stringBuilder.append(words[i]);
+
+                if ((i + 1 < words.length) &&
+                        (words[i + 1].length() > 0) &&
+                        Character.isUpperCase(words[i + 1].charAt(0)) &&
+                        ( !words[i].endsWith("."))){
+                    stringBuilder.append(". ");
+                }
+                else
+                    stringBuilder.append(" ");
+            }
+        }
+
+        System.out.println(stringBuilder.toString());
     }
 }

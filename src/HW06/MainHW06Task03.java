c5c57babaec328d2520f8ec24f23a0c5278d26c0
HOS <hos@ukr.net>
2019-04-12 12:07:16 +0300

    Completed task 3 from homework 06

--- src/HW06/MainHW06Task03.java
+++ src/HW06/MainHW06Task03.java
@@ -12,7 +12,24 @@
 package HW06;
 
 public class MainHW06Task03 {
+    static String toString(int[] array){
+        StringBuilder stringBuilder = new StringBuilder("int[] array [");
+
+        for (int i = 0; i < array.length; i++){
+            if (i < array.length - 1){
+                stringBuilder.append(array[i]).append(", ");
+            }
+            else {
+                stringBuilder.append(array[i]);
+            }
+        }
+
+        return stringBuilder.append("] ").toString();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        int[] array = {1, 2, 3, 4, 5};
+
+        System.out.println(toString(array));
     }
 }

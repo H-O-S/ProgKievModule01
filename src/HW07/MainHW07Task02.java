112b5b2b9a82c9f05940d26ea58897c8fb2c4d5b
HOS <hos@ukr.net>
2019-04-12 19:16:53 +0300

    Completed task 2 from homework 07

--- src/HW07/MainHW07Task02.java
+++ src/HW07/MainHW07Task02.java
@@ -2,7 +2,7 @@
  * Java Start. Home Work 07.
  *
  * @author Oleg Khrulov
- * @version dated 10.03.2019
+ * @version dated 12.04.2019
  */
 
 /*
@@ -14,6 +14,20 @@ package HW07;
 
 public class MainHW07Task02 {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        String word = "hello";
+        String text = "hello, it's me. hello from the outside. hello, how are you?";
+        int pos = 0;
+        int count = 0;
+
+        do {
+            pos = text.indexOf(word, pos);
+
+            if (pos >= 0){
+                count++;
+                pos += word.length();
+            }
+        }while (pos >= 0);
+
+        System.out.printf("The specified word is found %d times in this text.", count);
     }
 }

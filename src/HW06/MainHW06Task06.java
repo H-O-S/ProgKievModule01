f0bd6b0a46a05d423cf68f86861390f731f8aa4f
HOS <hos@ukr.net>
2019-04-12 13:30:44 +0300

    Completed task 6 from homework 06

--- src/HW06/MainHW06Task06.java
+++ src/HW06/MainHW06Task06.java
@@ -2,7 +2,7 @@
  * Java Start. Home Work 06.
  *
  * @author Oleg Khrulov
- * @version dated 19.03.2019
+ * @version dated 12.04.2019
  */
 
 /*
@@ -12,8 +12,34 @@
 
 package HW06;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class MainHW06Task06 {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter an integer:");
+
+            int integer = scanner.nextInt();
+            int enteredInteger = integer;
+            int count = 0;
+
+            while (integer != 0) {
+                if (integer % 2 > 0) {
+                    count++;
+                }
+
+                integer = integer / 2;
+            }
+
+            System.out.printf("The number of units in the binary representation %nof the entered integer %d is %s%n",
+                    enteredInteger, count);
+
+            scanner.close();
+
+        } catch (InputMismatchException e) {
+            System.out.println("Input Error!");
+        }
     }
 }

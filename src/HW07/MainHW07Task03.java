4f5a0641f8f994d496378334d25b5472c35f6e53
HOS <hos@ukr.net>
2019-04-12 19:32:20 +0300

    Completed task 3 from homework 07

--- src/HW07/MainHW07Task03.java
+++ src/HW07/MainHW07Task03.java
@@ -2,7 +2,7 @@
  * Java Start. Home Work 07.
  *
  * @author Oleg Khrulov
- * @version dated 10.03.2019
+ * @version dated 12.04.2019
  */
 
 /*
@@ -14,6 +14,18 @@ package HW07;
 
 public class MainHW07Task03 {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        int[] arrayOne = {1, 2, 3, 4, 5, 6};
+        int[] arrayTwo = {3, 4, 5, 6};
+        int[] arrayThree = {1, 2, 4, 5, 6};
+
+        int[][] arrayAll = {arrayOne, arrayTwo, arrayThree};
+        int sum = 0;
+
+        for (int i = 0; i < arrayAll.length; i++){
+            for (int j = 0; j < arrayAll[i].length; j++){
+                sum += arrayAll[i][j];
+            }
+        }
+        System.out.printf("Sum of elements in all arrays: %d", sum);
     }
 }
